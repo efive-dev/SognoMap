0532bc541c8a6be45ea408065428817c3860d60c
efive-dev <edoardo.quinto.dev@gmail.com>
2025-09-01 11:22:53 +0200

    Initial commit

---
+++ src/main/kotlin/efive/sognomap/SognoMapApplication.kt
@@ -0,0 +1,11 @@
+package efive.sognomap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SognoMapApplication
+
+fun main(args: Array<String>) {
+    runApplication<SognoMapApplication>(*args)
+}
---
+++ src/test/kotlin/efive/sognomap/SognoMapApplicationTests.kt
@@ -0,0 +1,13 @@
+package efive.sognomap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SognoMapApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
